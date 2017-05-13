95a95c10a1353b4c66c239583c55f3245765e942
Davi Eduardo <davieduardo94@gmail.com>
2017-05-12 21:21:47 -0400

    adc input range p tamanho produto

--- produto.php
+++ produto.php
@@ -39,6 +39,12 @@
                 <img src="img/produtos/foto2-azul.png" alt="Produto na cor azul">
               </label>
           </fieldset>
+          <fieldset  class="tamanhos">
+            <legend>Escolha o tamanho:</legend>
+
+            <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho" />
+          </fieldset>
+
 
           <input type="submit" class="comprar" value="Comprar">
         </form>
