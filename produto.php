73b4f94ee039a14097c013d467884a5aa4c5b3bd
Davi Eduardo <davieduardo94@gmail.com>
2017-05-23 13:05:07 -0400

    Configuracao de checkout de pagina

--- produto.php
+++ produto.php
@@ -21,7 +21,7 @@
         <h1>Fuzz Cardigan</h1>
         <p> por apenas R$ 129,00 </p>
 
-        <form>
+        <form action="checkout.php" method="POST">
           <fieldset class="cores">
               <legend> Escolha a cor: </legend>
 
