3e333fc31da5d923850ca91e70e11d4ea5236c91
Davi Eduardo <davieduardo94@gmail.com>
2017-05-25 21:41:40 -0400

    pagina checkout de compra

---
+++ checkout.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Checkout Mirror Fashion</title>
+    <meta name="viewport" content="width-device-width">
+</head>
+<body>
+  <h1> Ótima escolha!</h1>
+  <p>Obrigado pro comprar na Mirror Fashion!
+    Preencha seus daddos para efetivar a compra</p>
+
+    <h2> Sua compra</h2>
+    <dl>
+      <dt>Cor</dt>
+      <dd><?= $_POST['cor']?></dd>
+
+      <dt>Tamanho</dt>
+      <dd><?= $_POST['tamanho'] ?></dd>
+    </dl>
+
+
+</body>
+</html>
