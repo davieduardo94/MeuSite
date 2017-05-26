fb47bab333048960e22f1d700f165e038c1a54f3
Davi Eduardo <davieduardo94@gmail.com>
2017-05-26 17:17:44 -0400

    correcao de divs e posicionamento

--- checkout.php
+++ checkout.php
@@ -7,21 +7,24 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-
-    <div class="jumbotron">
+<div class="container">
+  <div class="jumbotron">
       <div class="container">
-    <h1> Ótima escolha!</h1>
-    <p>Obrigado pro comprar na Mirror Fashion!
-    Preencha seus dados para efetivar a compra</p>
-  </div>
+        <h1> Ótima escolha!</h1>
+        <p>Obrigado pro comprar na Mirror Fashion!
+        Preencha seus dados para efetivar a compra.</p>
+      </div> <!-- fim div container -->
+  </div><!--fim div jumbotron-->
+</div><!--fim div container -->
+
   <div class="container">
-    <div class="panel panel-default">
-      <div class="panel-heading">
-        <h2 class="panel-title">Sua compra</h2>
-    </div><!-- fim .panel-heading -->
-      <div class="panel-body">
-        <img src="img/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png"
-          class="img-thumbnail img-responsive">
+      <div class="panel panel-info">
+          <div class="panel-heading">
+            <h2 class="panel-title">Sua compra</h2>
+          </div><!-- fim .panel-heading -->
+        <div class="panel-body">
+          <img src="img/produtos/foto<?= $_POST["id"] ?>-<?= $_POST["cor"] ?> .png"
+              class="img-thumbnail img-responsive">
           <dl>
             <dt>Cor</dt>
             <dd><?=$_POST['cor']?></dd>
@@ -35,10 +38,10 @@
             <dt>Preço</dt>
             <dd><?= $_POST['preco'] ?> </dd>
           </dl>
-        </div>
-      </div>
+        </div> <!--fim .panel-body-->
+      </div><!--fim .panel-->
   </div>
-</div>
+
 
 </body>
 </html>
