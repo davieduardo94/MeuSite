97a4f5c99c0cd6d402093e8da69a55a993d5c06d
davieduardo94 <davieduardo94@gmail.com>
2017-05-26 00:51:25 -0400

    correcao bootstrap

--- checkout.php
+++ checkout.php
@@ -7,32 +7,36 @@
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
-  <div class="jumbotron">
-    <div class="container">
-  <h1> Ótima escolha!</h1>
-  <p>Obrigado pro comprar na Mirror Fashion!
-    Preencha seus daddos para efetivar a compra</p>
 
+    <div class="jumbotron">
+      <div class="container">
+    <h1> Ótima escolha!</h1>
+    <p>Obrigado pro comprar na Mirror Fashion!
+    Preencha seus dados para efetivar a compra</p>
+  </div>
+  <div class="container">
     <div class="panel panel-default">
       <div class="panel-heading">
         <h2 class="panel-title">Sua compra</h2>
     </div><!-- fim .panel-heading -->
-    <div class="panel-body">
-      <dl>
-        <dt>Cor</dt>
-        <dd><?=$_POST['cor']?></dd>
+      <div class="panel-body">
+        <img src="img/produtos/foto<?= $_POST['id'] ?>-<?= $_POST['cor'] ?>.png"
+          class="img-thumbnail img-responsive">
+          <dl>
+            <dt>Cor</dt>
+            <dd><?=$_POST['cor']?></dd>
 
-        <dt>Tamanho</dt>
-        <dd><?= $_POST['tamanho'] ?></dd>
+            <dt>Tamanho</dt>
+            <dd><?= $_POST['tamanho'] ?></dd>
 
-        <dt>Produto</dt>
-        <dd><?= $_POST['nome'] ?> </dd>
+            <dt>Produto</dt>
+            <dd><?= $_POST['nome'] ?> </dd>
 
-        <dt>Preço</dt>
-        <dd><?= $_POST['preco'] ?> </dd>
-      </dl>
+            <dt>Preço</dt>
+            <dd><?= $_POST['preco'] ?> </dd>
+          </dl>
+        </div>
       </div>
-    </div>
   </div>
 </div>
 
