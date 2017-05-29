f31ebb4e75511a3c907ae43e881f4650cba66577
davieduardo94 <davieduardo94@gmail.com>
2017-05-28 20:17:24 -0400

    criacao do formulario

--- checkout.php
+++ checkout.php
@@ -10,11 +10,11 @@
 <body>
 <div class="container">
   <div class="jumbotron">
-      <div class="container">
+      <!-- <div class="container"> -->
         <h1> Ótima escolha!</h1>
         <p>Obrigado pro comprar na Mirror Fashion!
         Preencha seus dados para efetivar a compra.</p>
-      </div> <!-- fim div container -->
+      <!-- </div> <!-- fim div container -->
   </div><!--fim div jumbotron-->
 </div><!--fim div container -->
 
@@ -24,9 +24,8 @@
             <h2 class="panel-title">Sua compra</h2>
           </div><!-- fim .panel-heading -->
         <div class="panel-body">
-
           <img src="img/produtos/foto1-verde.png" alt="Fuzzy Cardigan"
-class="img-thumbnail img-responsive"><!-- uma imagem estática sem envolver o PHP -->
+                    class="img-thumbnail img-responsive"><!-- uma imagem estática sem envolver o PHP -->
           <!-- <img src="img/produtos/foto<?= $_POST["id"] ?>-<?= $_POST["cor"] ?> .png"
               class="img-thumbnail img-responsive"> -->
           <dl>
@@ -44,6 +43,56 @@ class="img-thumbnail img-responsive"><!-- uma imagem estática sem envolver o PH
           </dl>
         </div> <!--fim .panel-body-->
       </div><!--fim .panel-->
+
+      <form><!-- inicio formulario -->
+        <fieldset>
+          <legend>Dados Pessoais</legend>
+          <div class="form-group">
+            <label for="nome">Nome Completo</label>
+            <input type="text" class="form-control" id="nome" name="nome" required="" autofocus>
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <div class="input-group">
+              <span class="input-group-addon">@</span>
+              <input type="email" class="form-control" id="email" name="email">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="cpf">CPF</label>
+            <input type="text" class="form-control" id="cpf" name="cpf" placeholder="000.000.000-00" required="">
+          </div>
+          <div class="checkbox">
+            <label>
+              <input type="checkbox" name="spam" value="sim" checked>
+              Quero receber spam da Mirror Fashion
+            </label>
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Cartao de Credito</legend>
+           <div class="form-group">
+             <label for="numeero-cartao">Numero - CVV</label>
+             <input type="text" class="form-control"  id="numero-cartao" name="numero-cartao">
+           </div>
+           <div class="form-group">
+             <label for="bandeira-cartao">Bandeira</label>
+             <select class="form-control" name="bandeira-cartao" id="bandeira-cartao">
+               <option value="master">MasterCard</option>
+               <option value="visa">Visa</option>
+               <option value="amex"> American Express</option>
+             </select>
+           </div>
+           <div class="form-group">
+             <label for="validade-cartao">Validade</label>
+             <input type="moth" class="form-control" id="validade-cartao" name="validade-cartao">
+           </div>
+        </fieldset>
+        <button type="suubmit" class="btn btn-lg btn-info ">
+          <span class="glyphicon glyphicon-thumbs-up"></span>
+          Confirmar Pedido
+        </button>
+      </form><!-- fim do formulario -->
   </div>
 
 
